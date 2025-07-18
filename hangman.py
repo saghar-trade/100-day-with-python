c1575b5848a7fd07fc031f831012e8e2a2f9d1ba
saghar <tajmilat.saghar@gmail.com>
2025-07-18 19:21:09 +0330

    hagman_word_list

---
+++ hangman.py
@@ -0,0 +1,13 @@
+word_list = [
+    "python", "developer", "algorithm", "function", "variable",
+    "syntax", "debugging", "iteration", "recursion", "exception",
+    "dictionary", "loop", "string", "boolean", "integer",
+    "inheritance", "polymorphism", "encapsulation", "framework", "library",
+    "object", "class", "module", "package", "parameter",
+    "argument", "binary", "hexadecimal", "compile", "runtime",
+    "virtual", "machine", "interpreter", "syntax", "keyword",
+    "array", "list", "tuple", "set", "queue",
+    "stack", "hashmap", "algorithm", "data", "structure",
+    "network", "protocol", "server", "client", "socket",
+    "encryption", "decryption", "security", "authentication", "authorization"
+]
